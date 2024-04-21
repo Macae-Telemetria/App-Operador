21476c7633ab5d9402d376e903b064fbf15c55a0
fonsecaBarreto <lucasbfonte@gmail.com>
2024-04-21 16:27:12 -0300

    refactor: splashscreenColor

--- lib/src/shared/styles.dart
+++ lib/src/shared/styles.dart
@@ -4,4 +4,4 @@ import 'package:flutter/material.dart';
 final primaryColor = Color.fromARGB(255, 1, 16, 42);
 final secondaryColor = const Color.fromARGB(255, 11, 61, 145);
 final backgroundColor = Color.fromARGB(255, 160, 50, 50);
-final splashScreenColor = Color.fromARGB(255, 160, 50, 50);
+final splashScreenColor = Color.fromARGB(255, 1, 16, 42);
